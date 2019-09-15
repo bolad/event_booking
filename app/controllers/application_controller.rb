eac40c01c9a4c320a45e98824cfe0c94e08a7b20
stanley akyea <stanley.akyea@gmail.com>
2019-09-15 23:32:23 +0200

    Add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
 
   def current_user
     @current_user ||= User.find_by(email: request.headers["X-User-Email"])
+  end
+
+  private
+
+  def authenticate_user
+    if current_user.blank?
+      render(json: { message: "Please login before making requests."} , status: :unprocessable_entity) && return
+    end
   end
 end
