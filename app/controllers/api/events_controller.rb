6f71cacb204e5ff74157bf8ba05444b59fc2cbbb
stanley akyea <stanley.akyea@gmail.com>
2019-09-15 21:49:32 +0200

    Add show and index actions

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,21 @@
 class Api::EventsController < ApplicationController
+  before_action :set_event, only: [:show, :available_tickets ]
 
+  def show
+    render json: @event
+  end
+
+  def index
+    @events = Event.all
+    render json: @events, status: :ok
+  end
 
   def available_tickets
   end
 
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
