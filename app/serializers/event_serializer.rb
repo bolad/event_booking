9b3675a8450244f716c0e343ab626f08efcf86b6
stanley akyea <stanley.akyea@gmail.com>
2019-09-16 02:37:48 +0200

    Parse event date and time properly

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,15 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_at, :alloted_tickets, :sold_out_tickets
+  attributes :id, :name, :alloted_tickets, :sold_out_tickets, :start_date, :start_time
 
   has_many :bookings
   has_many :users
+
+  def start_date
+    object.start_at.strftime("%d.%m.%Y")
+  end
+
+  def start_time
+    object.start_at.strftime("%I:%M%p")
+  end
 
 end
