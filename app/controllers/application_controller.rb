fd1f822014d1a9239d2385e4848e47c33c886fde
stanley akyea <stanley.akyea@gmail.com>
2020-09-21 15:45:27 +0200

    Configure session store to work in all environments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,3 @@
 class ApplicationController < ActionController::API
-  # before_action :authenticate_user
-  #
-  # def current_user
-  #   @current_user ||= User.find_by(email: request.headers["X-User-Email"])
-  # end
-  #
-  # private
-  #
-  # def authenticate_user
-  #   if current_user.blank?
-  #     render(json: { message: "Please login before making requests."} , status: :unprocessable_entity) && return
-  #   end
-  # end
-
   skip_before_action :verify_authenticity_token, raise: false
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_event_app", domain: "bld-event-api.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_event_app", domain: "bld-event-api.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_event_app"
+end
